7f0796aae60d0c6f7983a94ca00196bfbb8469cd
beni69 <73666440+beni69@users.noreply.github.com>
2021-05-20 16:04:11 +0200

    :sparkles: redesigned profile page

--- next/pages/profile.tsx
+++ next/pages/profile.tsx
@@ -1,32 +1,33 @@
-import auth from "../lib/auth";
+import {
+    Box,
+    Center,
+    Flex,
+    VStack,
+    Text,
+    Heading,
+    Avatar,
+} from "@chakra-ui/react";
+import Card from "../components/card";
+import auth, { withAuth } from "../lib/auth";
 
-const Profile = ({ user }) => {
-    if (!user)
-        return (
-            <div>
-                <h2>not logged in</h2>
-            </div>
-        );
-
-    console.log({ user });
+const Profile = ({ user }: { user: firebase.default.User }) => {
+    if (!user) return null;
 
     return (
-        <main>
-            <ul>
-                <li>{user.displayName}</li>
-                <li>{user.email}</li>
-                <li>{user.uid}</li>
-                {user.photoURL && (
-                    <li>
-                        <img src={user.photoURL} alt="user's profile pic" />
-                    </li>
-                )}
-            </ul>
-            <button onClick={() => auth.signOut()}>sign out</button>
-        </main>
+        <Center as="main" h={["69vh", null, "80vh"]}>
+            <VStack as={Card} p={24}>
+                <Avatar
+                    size={"xl"}
+                    name={user.displayName}
+                    src={user.photoURL}
+                />
+                <Heading>{user.displayName}</Heading>
+                <Text>{user.email}</Text>
+            </VStack>
+        </Center>
     );
 };
 
 Profile.pageName = "Profile";
 
-export default Profile;
+export default withAuth(Profile);
