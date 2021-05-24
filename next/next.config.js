378fcdb192f92c286a73cfc7e589fb2d1becd1d0
beni69 <73666440+beni69@users.noreply.github.com>
2021-05-24 14:24:11 +0200

    Revert ":heavy_plus_sign: next-pwa"
    This reverts commit 5eb9d9106d45cc14b7947bef5b316575851eb50d.

--- next/next.config.js
+++ next/next.config.js
@@ -1,11 +1,5 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-    pwa: {
-        dest: "public",
-    },
-
+module.exports = {
     future: {
         webpack5: true,
     },
-});
+};
