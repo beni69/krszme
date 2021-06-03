4a52ed164cdc8eeadef4cb31d61f049774b1e0b2
beni69 <73666440+beni69@users.noreply.github.com>
2021-06-03 12:37:51 +0200

    :sparkles: implementing analytics
    events for creating/deleting links and page views

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,23 +1,38 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useState, useRef } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 import { IconContext } from "react-icons";
 import Footer from "../components/footer";
 import Loader from "../components/loader";
 import Nav from "../components/nav";
+import { logPage } from "../lib/analytics";
 import auth, { AuthContext } from "../lib/auth";
 import theme from "../lib/theme";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const PROD = process.env.NODE_ENV === "production";
+    if (typeof window !== "undefined") console.log({ PROD });
+
+    const router = useRouter();
     const [user, setUser] = useState<firebase.default.User>(undefined);
-
     auth.onAuthStateChanged(setUser);
-
     const footerHeight = ["8rem", null, "3.5rem"];
-
     const navRef = useRef(null);
+
+    useEffect(() => {
+        if (!PROD) return;
+
+        router.events.on("routeChangeComplete", logPage);
+
+        logPage(window.location.pathname);
+
+        return () => {
+            router.events.off("routeChangeComplete", logPage);
+        };
+    }, []);
 
     return (
         <>
