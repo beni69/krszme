2655a523405f018d072a94d9836390650598230a
beni69 <73666440+beni69@users.noreply.github.com>
2021-06-03 12:37:01 +0200

    :sparkles: analytics logic

---
+++ next/lib/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyDx2VXjSakAWoV7JszU6PYZgfJX5OZKOME",
+    authDomain: "krszme.firebaseapp.com",
+    projectId: "krszme",
+    storageBucket: "krszme.appspot.com",
+    messagingSenderId: "1002758976981",
+    appId: "1:1002758976981:web:b2c10e4a823e3fcb4d795c",
+    measurementId: "G-V0F4B0H7M9",
+};
+
+export const app = firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp(config);
+
+export default app;
